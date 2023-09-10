ae8252a4c4e2b92ff8c3beeb99c1d9cef8269855
Derek.Nguyen <derek.nguyen@avepoint.com>
2023-09-10 21:18:14 +0700

    SecondSolution

--- DerekAnalyser/DerekAnalyser.cs
+++ DerekAnalyser/DerekAnalyser.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Contract;
 using System.IO;
+using Microsoft.VisualBasic;
 
     public class DerekAnalyser : IDataAnalyser
     {
@@ -19,9 +20,11 @@ using System.IO;
         public IEnumerable<string> GetTopTenStrings(string path)
         {
             string[] result = { };
-            try
+            List<int> hashcodeString = new List<int>();
+        Dictionary<int, int> mp = new Dictionary<int, int>();
+        try
             {
-                Dictionary<int, byte> wordFrequency = new Dictionary<int, byte>();
+               // Dictionary<int, byte> wordFrequency = new Dictionary<int, byte>();
 
                 string[] datFiles = Directory.GetFiles(path, "*.dat");
                 foreach (string filePath in datFiles)
@@ -34,47 +37,57 @@ using System.IO;
                         while ((line = reader.ReadLine()) != null)
                         {
                             var words = line.Split(';', StringSplitOptions.RemoveEmptyEntries);
+                           
                             foreach (var word in words)
                             {
                                 string normalizedWord = word.ToLower();
-                                if (wordFrequency.ContainsKey(normalizedWord.GetHashCode()))
-                                {
-                                    wordFrequency[normalizedWord.GetHashCode()]++;
-                                }
-                                else
-                                {
-                                    wordFrequency[normalizedWord.GetHashCode()] = 1;
-                                }
+                            if (mp.ContainsKey(normalizedWord.GetHashCode()))
+                            {
+                                mp[normalizedWord.GetHashCode()]++;
                             }
+                            else
+                            {
+                                mp[normalizedWord.GetHashCode()] = 1;
+                            }
+
                         }
+                        
+                    }
 
                         fs.Close();
                         reader.Close();
                     }
-
-                }
-
-                var top10Words = wordFrequency
-                     .Where(entry => entry.Value >1)
-                    .OrderByDescending(s => s.Value)
-                    .Take(10)
-                    .Select(entry => entry.Key);
-                List<string> frequentWords = new List<string>();
-                foreach (var word in top10Words)
+             
+            }
+            List<int> queue = mp.Keys.ToList();
+            queue.Sort(delegate (int y, int x) {
+                if (mp[x] == mp[y])
+                    return x.CompareTo(y);
+                else
+                    return (mp[x]).CompareTo(mp[y]);
+            });
+            Console.WriteLine(  " numbers with the most occurrences are:");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(queue[i] + $" occurs {mp[queue[i]] }");
+            }
+            //Console.WriteLine( " numbers with the most occurrences are:");
+            List<string> frequentWords = new List<string>();
+            for (int i = 0; i < 10; i++)
+            {
+                foreach (string filePath in datFiles)
                 {
-                    foreach (string filePath in datFiles)
+                    using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                    using (StreamReader reader = new StreamReader(fs))
                     {
-                        using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
-                        using (StreamReader reader = new StreamReader(fs))
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
                         {
-                            string line;
-                            while ((line = reader.ReadLine()) != null)
+                            var words = line.Split(';', StringSplitOptions.RemoveEmptyEntries);
+                            foreach (var wordInDatFiles in words)
                             {
-                                var words = line.Split(';', StringSplitOptions.RemoveEmptyEntries);
-                                foreach (var wordInDatFiles in words)
-                                {
-                                    string normalizedWord = wordInDatFiles.ToLower();
-                                if (word == normalizedWord.GetHashCode())
+                                string normalizedWord = wordInDatFiles.ToLower();
+                                if (queue[i] == normalizedWord.GetHashCode())
                                 {
                                     if (!frequentWords.Contains(normalizedWord))
                                     {
@@ -82,19 +95,100 @@ using System.IO;
 
                                     }
                                 }
-                                }
-                            
                             }
+
+                        }
                         fs.Close();
                         reader.Close();
                     }
-                  
                 }
-                }
+            }
+            //List<int> frequentNumbers = GetMostFrequentNumbers(hashcodeString, 10);
+            //foreach (int number in frequentNumbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+            //var top10Words = hashcodeString
+            //.GroupBy(word => word)
+            //.ToDictionary(g => g.Key, g => g.Count())
+            //.OrderByDescending(o => o.Value)
+            //.Take(10)
+            //.Select(n => n.Key)
+            //.ToList();
+            //foreach (var item in top10Words)
+            //{
+
+            //        Console.WriteLine(item);
+            //}
+            //foreach (var item in top10Words)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            //Dictionary<int, byte> frequencyDict = new Dictionary<int, byte>();
+            //int currentNumber = hashcodeString[0];
+            //byte count = 1;
+
+            //for (int i = 0; i < hashcodeString.Count; i++)
+            //{
+            //    if (hashcodeString[i] == currentNumber)
+            //    {
+            //        count++;
+            //    }
+            //    else
+            //    {
+            //        frequencyDict[currentNumber] = count;
+            //        currentNumber = hashcodeString[i];
+            //        count = 1;
+            //    }
+            //}
+            //frequencyDict[currentNumber] = count;
+            //var top10Words = frequencyDict
+            //    .OrderByDescending(x => x.Value)
+            //    .Take(10)
+            //    .Select(x => x.Key);
+            //foreach (var item in top10Words)
+            //{
+            //    Console.WriteLine($"The {item} occurs {frequencyDict[item]}");
+            //}
+
+            ////List<string> frequentWords = new List<string>();
+            //foreach (var word in queue)
+            //{
+            //    foreach (string filePath in datFiles)
+            //    {
+            //        using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            //        using (StreamReader reader = new StreamReader(fs))
+            //        {
+            //            string line;
+            //            while ((line = reader.ReadLine()) != null)
+            //            {
+            //                var words = line.Split(';', StringSplitOptions.RemoveEmptyEntries);
+            //                foreach (var wordInDatFiles in words)
+            //                {
+            //                    string normalizedWord = wordInDatFiles.ToLower();
+            //                    if (word == normalizedWord.GetHashCode())
+            //                    {
+            //                        if (!frequentWords.Contains(normalizedWord))
+            //                        {
+            //                            frequentWords.Add(normalizedWord);
+
+            //                        }
+            //                    }
+            //                }
+
+            //            }
+            //            fs.Close();
+            //            reader.Close();
+            //        }
+
+            //    }
+            //}
 
 
 
-                return frequentWords;
+            return frequentWords;
 
 
             }
@@ -105,8 +199,10 @@ using System.IO;
             return result;
 
         }
-        public DerekAnalyser(string path)
+    
+    public DerekAnalyser(string path)
         {
             this.Path = path;
         }
+
     }
