accf701eb2f336761d844dce47719c78b292a8b5
Derek.Nguyen <derek.nguyen@avepoint.com>
2023-09-10 21:20:03 +0700

    Update-Delete Comment

--- DerekAnalyser/DerekAnalyser.cs
+++ DerekAnalyser/DerekAnalyser.cs
@@ -20,11 +20,11 @@ using Microsoft.VisualBasic;
         public IEnumerable<string> GetTopTenStrings(string path)
         {
             string[] result = { };
-            List<int> hashcodeString = new List<int>();
+
         Dictionary<int, int> mp = new Dictionary<int, int>();
         try
             {
-               // Dictionary<int, byte> wordFrequency = new Dictionary<int, byte>();
+               
 
                 string[] datFiles = Directory.GetFiles(path, "*.dat");
                 foreach (string filePath in datFiles)
@@ -71,7 +71,7 @@ using Microsoft.VisualBasic;
             {
                 Console.WriteLine(queue[i] + $" occurs {mp[queue[i]] }");
             }
-            //Console.WriteLine( " numbers with the most occurrences are:");
+           
             List<string> frequentWords = new List<string>();
             for (int i = 0; i < 10; i++)
             {
@@ -103,89 +103,7 @@ using Microsoft.VisualBasic;
                     }
                 }
             }
-            //List<int> frequentNumbers = GetMostFrequentNumbers(hashcodeString, 10);
-            //foreach (int number in frequentNumbers)
-            //{
-            //    Console.WriteLine(number);
-            //}
-            //var top10Words = hashcodeString
-            //.GroupBy(word => word)
-            //.ToDictionary(g => g.Key, g => g.Count())
-            //.OrderByDescending(o => o.Value)
-            //.Take(10)
-            //.Select(n => n.Key)
-            //.ToList();
-            //foreach (var item in top10Words)
-            //{
-
-            //        Console.WriteLine(item);
-            //}
-            //foreach (var item in top10Words)
-            //{
-            //    Console.WriteLine(item);
-            //}
-
-
-            //Dictionary<int, byte> frequencyDict = new Dictionary<int, byte>();
-            //int currentNumber = hashcodeString[0];
-            //byte count = 1;
-
-            //for (int i = 0; i < hashcodeString.Count; i++)
-            //{
-            //    if (hashcodeString[i] == currentNumber)
-            //    {
-            //        count++;
-            //    }
-            //    else
-            //    {
-            //        frequencyDict[currentNumber] = count;
-            //        currentNumber = hashcodeString[i];
-            //        count = 1;
-            //    }
-            //}
-            //frequencyDict[currentNumber] = count;
-            //var top10Words = frequencyDict
-            //    .OrderByDescending(x => x.Value)
-            //    .Take(10)
-            //    .Select(x => x.Key);
-            //foreach (var item in top10Words)
-            //{
-            //    Console.WriteLine($"The {item} occurs {frequencyDict[item]}");
-            //}
-
-            ////List<string> frequentWords = new List<string>();
-            //foreach (var word in queue)
-            //{
-            //    foreach (string filePath in datFiles)
-            //    {
-            //        using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
-            //        using (StreamReader reader = new StreamReader(fs))
-            //        {
-            //            string line;
-            //            while ((line = reader.ReadLine()) != null)
-            //            {
-            //                var words = line.Split(';', StringSplitOptions.RemoveEmptyEntries);
-            //                foreach (var wordInDatFiles in words)
-            //                {
-            //                    string normalizedWord = wordInDatFiles.ToLower();
-            //                    if (word == normalizedWord.GetHashCode())
-            //                    {
-            //                        if (!frequentWords.Contains(normalizedWord))
-            //                        {
-            //                            frequentWords.Add(normalizedWord);
-
-            //                        }
-            //                    }
-            //                }
-
-            //            }
-            //            fs.Close();
-            //            reader.Close();
-            //        }
-
-            //    }
-            //}
-
+           
 
 
             return frequentWords;
